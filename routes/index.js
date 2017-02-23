a74969c762318af0d5614cb2d7849300f9238058
Nikita Allin <nikita.allin@swisstok.ru>
2017-02-23 17:29:38 +0300

    Добавил комментарии

--- routes/index.js
+++ routes/index.js
@@ -796,9 +796,9 @@ function getMapInfo(type, token, serverDomain, timeForGame) {
                                                     }
                                                 ],
                                                 "fieldMessage":{
-                                                    "text":"9ка",
+                                                    "text":"",
                                                     "type":4,
-                                                    "duration":48,
+                                                    "duration":0,
                                                     "cellId":obj.id,
                                                     "targetId":31
                                                 }
@@ -846,9 +846,9 @@ function getMapInfo(type, token, serverDomain, timeForGame) {
                                                     }
                                                 ],
                                                 "fieldMessage":{
-                                                    "text":"15ка",
+                                                    "text":"",
                                                     "type":4,
-                                                    "duration":48,
+                                                    "duration":0,
                                                     "cellId":obj.id,
                                                     "targetId":31
                                                 }
@@ -1362,29 +1362,35 @@ function autoFarmFinder(name, xCor, yCor, filter) {
 //     "session": token
 // };
 
+/**
+ * Добавления юнитов по улсовиям
+ */
 // autoFarmFinder('wkf', '-2', '-5', withoutKingdomsFilter);
 // autoFarmFinder('d', '-2', '-5', deathsFilter);
 
-//Вынести это в файл инцирования
-// autoFarmList(3600, 300, listPayload.Sobol, 'rux3', false);
-// autoFarmList(1500, 300, listPayload.GreedyKs1, 'ks1-com', true);
-// autoFarmList(1500, 600, listPayload.GROM, 'ks1-com', true);
+/**
+ * Фармлисты
+ */
 // autoFarmList(2400, 1200, listPayload.Wahlberg , 'rux3', true);
 // autoFarmList(2400, 1200, listPayload.Wahlberg2, 'rux3', true);
 // autoFarmList(2400, 1200, listPayload.Wahlberg3, 'rux3', true);
 // autoFarmList(2400, 1200, listPayload.Wahlberg4, 'rux3', true);
-// autoFarmList(3600, 800, listPayload.cheetah_1, 'rux3', true);
-// autoFarmList(3600, 1200, listPayload.cheetah_2, 'rux3', true);
-// autoFarmList(3600, 2400, listPayload.cheetah_3, 'rux3', true);
 
-let repeatFn = function(){
- getMapInfo('animal', token, serverDomain, timeForGame);
- setTimeout(repeatFn, 600000);
-};
+/**
+ * Крокодилы
+ */
+// let repeatFn = function(){
+//  getMapInfo('animal', token, serverDomain, timeForGame);
+//  setTimeout(repeatFn, 600000);
+// };
 
-getMapInfo('animal', token, serverDomain, timeForGame);
-// getMapInfo('crop', token, serverDomain, timeForGame);
-setTimeout(repeatFn, 600000);
+// getMapInfo('animal', token, serverDomain, timeForGame);
+
+/**
+ * Кроп
+ */
+getMapInfo('crop', token, serverDomain, timeForGame);
+// setTimeout(repeatFn, 600000);
 
 
 
