4e12c136d6d9be2de81710b9359dae323daa037a
Alex Larchenko <alarchenko@callfire.com>
2023-02-04 04:32:39 +0200

    test

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 function Users() {
 
     this.Coss = {
-        session: '48ec9b5e62d10e588f67',
+        session: '57414462fac5be33985b',
+        clientId: '63dd7c7e21f77',
+        cookie: '__cmpconsentx17155=CPmPa7APmPa7AAfSDBENC1CgAP_AAH_AAAigJLNf_X__b3_v-_7___t0eY1f9_7__-0zjhfdt-8N3f_X_L8X_2M7vF36tr4KuR4ku3bBIUdtHOncTUmx6olVrzPsb02cr7NKJ_Pkmnsbe2dYGH9_n9_z-ZKZ7______7__________________________________________________________________9___BJZr_6__7e_9_3_f__9ujzGr_v_f__2mccL7tv3hu7_6_5fi_-xnd4u_VtfBVyPEl27YJCjto507iak2PVEqteZ9jemzlfZpRP58k09jb2zrAw_v8_v-fzJTPf______f__________________________________________________________________v__4AA; __cmpcccx17155=aBPmR99-gAwAzADcDuAAIABgAHgAUABYADoALgAwABqADoAPQAgACKAEgASgAoABcADEAGgAPIAgACCAEkAJoAVwAvQB7AHwAQ4AxACCgEaAI6ATgA0IByQG-gOCAcSA5IB5YD0QIOAQ2AiSBEwCKAFgQLMgWiAt6BcEC4YGagM8AahA68CHQERAI3wSMAkoBMECY4E3gKFAUaAIBdVC72F8WL-4v9BkujKsMr4cig6ZiAjF2KUFEqa1tvG3Ctw63EFuIm4jtyU3JYA; _ga=GA1.2.564880542.1674837212; _gid=GA1.2.479764944.1675458996; gl5SessionKey=%7B%22key%22%3A%2233601cff0e15db5abbfa%22%2C%22id%22%3A%221142177%22%7D; gl5PlayerId=1142177; msid=13nmg9hnmonusel1phtkbc4421; t5SessionKey=%7B%22key%22%3A%2257414462fac5be33985b%22%2C%22id%22%3A%22249%22%7D; t5mu=5RmxXKsJ5vaI; village=538394616',
         village: '538394616',
         coords: { x: -8, y: 46 },
-        clientId: '63dcecf62bb7e',
-        //village2: '536690708',
+        village2: '538427383',
         //village3: '536789008',
         //village4: '536723475',
         // village5: '536592403',
@@ -17,17 +18,16 @@ function Users() {
         // village11: '536952837',
         // village12: '537051161',
         // village13: '536625187',
-        serverDomain: 'testx5',
-        cookie: '_ga=GA1.2.446006017.1668635635; __cmpconsentx17155=CPmSt3APmSt3AAfSDBENC1CgAP_AAH_AAAigJLNf_X__b3_v-_7___t0eY1f9_7__-0zjhfdt-8N3f_X_L8X_2M7vF36tr4KuR4ku3bBIUdtHOncTUmx6olVrzPsb02cr7NKJ_Pkmnsbe2dYGH9_n9_z-ZKZ7______7__________________________________________________________________9___BJZr_6__7e_9_3_f__9ujzGr_v_f__2mccL7tv3hu7_6_5fi_-xnd4u_VtfBVyPEl27YJCjto507iak2PVEqteZ9jemzlfZpRP58k09jb2zrAw_v8_v-fzJTPf______f__________________________________________________________________v__4AA; __cmpcccx17155=aBPmSxPogAwAzADcDuAAIABgAHgAUABYADoALgAwABqADoAPQAgACKAEgASgAoABcADEAGgAPIAgACCAEkAJoAVwAvQB7AHwAQ4AxACCgEaAI6ATgA0IByQG-gOCAcSA5IB5YD0QIOAQ2AiSBEwCKAFgQLMgWiAt6BcEC4YGagM8AahA68CHQERAI3wSMAkoBMECY4E3gKFAUaAIBdVC72F8WL-4v9BkujKsMr4cig6ZiAjF2KUFEqa1tvG3Ctw63EFuIm4jtyU3JYA; _gid=GA1.2.1065979640.1675273455; t5mu=c55EkSin6OlZ; gl5SessionKey=%7B%22key%22%3A%22dbd7e34ccc53f2d1baaf%22%2C%22id%22%3A%221142177%22%7D; gl5PlayerId=1142177; msid=4hgsuo6sgmfhddl23ucj7oakg6; t5SessionKey=%7B%22key%22%3A%2248ec9b5e62d10e588f67%22%2C%22id%22%3A%22249%22%7D; village=538394616'
+        serverDomain: 'testx5'
     };
 
     this.AlexCoss = {
-        session: 'e58f3bae365c00962c39',
+        session: 'bed92f4f147b4430fd49',
+        clientId: '63dd7e0726b74',
+        cookie: '__cmpcc=1; __cmpconsentx17155=CPmmffAPmmffAAfSDBENC2CgAAAAAAAAAAigAAAAAAAA; __cmpcccx17155=aBPmpsrwgAAAAAA; gl5SessionKey=%7B%22key%22%3A%22a2e027af3bab09f7843a%22%2C%22id%22%3A%221140788%22%7D; gl5PlayerId=1140788; msid=ibeo2njvd1evn7qsr459952t00; t5SessionKey=%7B%22key%22%3A%22bed92f4f147b4430fd49%22%2C%22id%22%3A%22833%22%7D; t5mu=iM5A4LBeHbrs; village=536625141',
         village: '536625141',
         coords: { x: -11, y: -8 },
-        clientId: '63dd28f9d257c',
-        serverDomain: 'es2x3',
-        cookie: '__cmpcc=1; __cmpconsentx17155=CPmmffAPmmffAAfSDBENC2CgAP_AAH_AAAigJLNf_X__b3_v-_7___t0eY1f9_7__-0zjhfdt-8N3f_X_L8X_2M7vF36tr4KuR4ku3bBIUdtHOncTUmx6olVrzPsb02cr7NKJ_Pkmnsbe2dYGH9_n9_z-ZKZ7______7__________________________________________________________________9___BJZr_6__7e_9_3_f__9ujzGr_v_f__2mccL7tv3hu7_6_5fi_-xnd4u_VtfBVyPEl27YJCjto507iak2PVEqteZ9jemzlfZpRP58k09jb2zrAw_v8_v-fzJTPf______f__________________________________________________________________v__4AA; __cmpcccx17155=aBPmo5e-gAwAzADcDuAAIABgAHgAUABYADoALgAwABqADoAPQAgACKAEgASgAoABcADEAGgAPIAgACCAEkAJoAVwAvQB7AHwAQ4AxACCgEaAI6ATgA0IByQG-gOCAcSA5IB5YD0QIOAQ2AiSBEwCKAFgQLMgWiAt6BcEC4YGagM8AahA68CHQERAI3wSMAkoBMECY4E3gKFAUaAIBdVC72F8WL-4v9BkujKsMr4cig6ZiAjF2KUFEqa1tvG3Ctw63EFuIm4jtyU3JYA; _ga=GA1.2.342668809.1675438279; _gid=GA1.2.202867293.1675438279; _fbp=fb.1.1675438278981.1367452248; gl5SessionKey=%7B%22key%22%3A%222f3bd0cbf52c11150ec0%22%2C%22id%22%3A%221140788%22%7D; gl5PlayerId=1140788; msid=3lo2v1opag88dg3diiglvqdue6; t5SessionKey=%7B%22key%22%3A%22e58f3bae365c00962c39%22%2C%22id%22%3A%22833%22%7D; t5mu=i1E9wAi1Voqc; village=536625141'
+        serverDomain: 'es2x3'
     };
 
     this.Acrom = {
